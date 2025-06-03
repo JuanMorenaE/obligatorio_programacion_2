802da76050b2cb54d7416566dbd180598bf7a8e9
andres <abellini@correo.um.edu.uy>
2025-06-03 12:10:32 -0300

    Carpeta inicial

--- src/entities/Generos.java
+++ src/entities/Generos.java
@@ -3,14 +3,19 @@ package entities;
 public class Generos {
     private int id;
     private String nombre;
+    private int visitas;
+
 
     public Generos(int id, String nombre) {
         this.id = id;
         this.nombre = nombre;
+        this.visitas = 0;
     }
 
     public int getId() {return id;}
     public String getNombre() {return nombre;}
+    public int getVisitas() {return visitas;}
 
+    public void agregarVisita(){visitas++;}
 
 }
