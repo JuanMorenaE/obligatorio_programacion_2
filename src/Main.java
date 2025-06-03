58ac6875b92a84135fc3adea9a20515d7326787f
Juan Andrés Morena <juanmorenae@gmail.com>
2025-06-03 10:40:00 -0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,10 @@
-import netscape.javascript.JSObject;
+import utils.DataLoading;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        try (Scanner scanner = new Scanner(new File("src/movies_metadata.csv"))) {
-            int lines = 0;
-
-            // Leer cada línea del archivo
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine(); // Avanza una línea
-                lines++;            // Cuenta la línea
-            }
-
-            System.out.println("¡Finalizado! Total de líneas: " + lines);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        DataLoading.LoadMovies();
     }
 }
---
+++ src/TADs/List/ListNode.java
@@ -0,0 +1,10 @@
+package TADs.List;
+
+public class ListNode<T> {
+    public T item;
+    public ListNode<T> next;
+
+    public ListNode(T item) {
+        this.item = item;
+    }
+}
