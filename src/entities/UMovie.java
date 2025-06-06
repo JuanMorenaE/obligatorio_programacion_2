e1f30da9057b7328c0e55c392ba258bac350719d
andres <abellini@correo.um.edu.uy>
2025-06-06 11:26:41 -0300

    Carpeta inicial

---
+++ src/entities/UMovie.java
@@ -0,0 +1,20 @@
+package entities;
+
+import TADs.Hash.HashLinkedList;
+
+import java.util.Hashtable;
+
+public class UMovie {
+    private HashLinkedList peliculas;
+    private HashLinkedList actores;
+    private HashLinkedList directores;
+    private HashLinkedList ratings;
+    private HashLinkedList usuarios;
+    private HashLinkedList generos;
+
+
+
+
+
+
+}
